40cd0bc7ffb58aaaf6ecca9666c206b275359e48
gimarcus1971 <90014005+gimarcus1971@users.noreply.github.com>
2023-05-26 13:24:16 -0300

    New Build_6

--- src/main/java/com/portfolio/gimarcus/WebConfig.java
+++ src/main/java/com/portfolio/gimarcus/WebConfig.java
@@ -4,11 +4,14 @@
  */
 package com.portfolio.gimarcus;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
@@ -25,17 +28,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
 */
-
 @Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*");
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http.antMatcher("/**").authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+    http.cors().and().csrf().disable();
+}
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedMethods("*");
+        }
+    }; 
   }
-} 
+}
