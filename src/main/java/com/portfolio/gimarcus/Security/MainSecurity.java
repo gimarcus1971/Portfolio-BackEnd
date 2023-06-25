edc347d255522cf7a71bcceb99f7c028e12cd01a
gimarcus1971 <90014005+gimarcus1971@users.noreply.github.com>
2023-06-25 12:30:45 -0300

    CORS NewBuild-27

--- src/main/java/com/portfolio/gimarcus/Security/MainSecurity.java
+++ src/main/java/com/portfolio/gimarcus/Security/MainSecurity.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,7 +31,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class MainSecurity {
     @Autowired
     UserDetailsImpl userDetailsServiceImpl;
@@ -60,7 +60,7 @@ public class MainSecurity {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
